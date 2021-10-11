ad3635bf57853b0c2a3ca855336d30c9780a34a6
PhilipK-webdev <philipk179@gmail.com>
2021-10-11 12:14:02 +0300

    ADD:added API and functionality to the card

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import CardQuestion from "./components/CardQuestion";
 import Header from "./components/Header";
-
+import axios from "axios"
 function App() {
   const [idClick, setIdClick] = useState();
+  const [objSport, setObjSport] = useState({});
+  const [objMusic, setObjMusic] = useState({});
+  const [objFilm, setObjFilm] = useState({});
+  const [objTelevision, setObjTelevsion] = useState({});
   const clickHandler = (e) => {
     e.preventDefault();
     setIdClick(e.currentTarget.dataset.id);
   }
+
+  useEffect(() => {
+    generateAPI(idClick);
+  }, [idClick])
+  const generateAPI = (idClick) => {
+    if (idClick != undefined) {
+      switch (idClick) {
+        case "1":
+          sportAPI();
+          break;
+        case "2":
+          musicAPI();
+          break;
+        case "3":
+          filmAPI();
+          break;
+        case "4":
+          televisionAPI();
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  const sportAPI = () => {
+    console.log("SPORT");
+    axios.get("https://opentdb.com/api.php?amount=10&category=21&type=multiple").then(({ data: { results } }) => {
+      setObjSport(results);
+      console.log(results);
+    }).catch(err => console.log(err))
+  }
+  const musicAPI = () => {
+    console.log("MUSIC");
+    axios.get("https://opentdb.com/api.php?amount=10&category=12&type=multiple").then(({ data: { results } }) => { setObjMusic(results) }).catch(err => console.log(err))
+  }
+  const filmAPI = () => {
+    console.log("FILM");
+    axios.get("https://opentdb.com/api.php?amount=10&category=11").then(({ data: { results } }) => setObjFilm(results)).catch(err => console.log(err))
+  }
+  const televisionAPI = () => {
+    console.log("TELE");
+    axios.get("https://opentdb.com/api.php?amount=10&category=14").then(({ data: { results } }) => setObjTelevsion(results)).catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
       <Header clickHandler={clickHandler} />
       <div className="card-question">
-        <CardQuestion idChoose={idClick} />
+        <CardQuestion objSport={objSport} />
       </div>
     </div>
   );
--- src/components/CardQuestion.js
+++ src/components/CardQuestion.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../components/CardQuestion.css";
-function CardQuestion({ idChoose }) {
+function CardQuestion({ objSport }) {
+    const [question, setQuestion] = useState();
+    const [answers, setAnswers] = useState([]);
+    const [correctAnswer, setCorrectAnswer] = useState("");
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        if (Object.keys(objSport).length !== 0) {
+            setQuestion(objSport[index].question.replace(/[^a-zA-Z ]/g, ""));
+            setAnswers([...objSport[index].incorrect_answers, objSport[index].correct_answer]);
+            setCorrectAnswer(objSport[index].correct_answer);
+        }
+    }, [objSport])
+    const nextQuestion = (e) => {
+        e.preventDefault();
+        setIndex(index + 1);
+        if (index < 10) {
+            setQuestion(objSport[index].question.replace(/[^a-zA-Z ]/g, ""));
+            setAnswers([...objSport[index].incorrect_answers, objSport[index].correct_answer]);
+            setCorrectAnswer(objSport[index].correct_answer);
+        } else {
+            setQuestion("GAME OVER");
+            setAnswers("");
+            setCorrectAnswer("")
+        }
+    }
     return (
-        <div className="quiz">
-            <div className="quiz__question">
-                <h1><span><i className="fa fa-question-circle" aria-hidden="true"></i>
-                </span>What is the Capital of Jamaica?</h1>
+        <div>
+            <button className="btn-next" onClick={nextQuestion}>Next</button>
+            <div className="quiz">
+                <div className="quiz__question">
+                    <h1><span><i className="fa fa-question-circle" aria-hidden="true"></i>
+                    </span>{!question ? "Loading" : <span>{question}<ul>
+                        {answers !== "" ? answers.map((res, index) => (
+                            <li li key={index}>{res}</li>
+                        )) : <li></li>}
+                    </ul></span>}</h1>
+
+                </div>
+                <div className="quiz__answer">
+                    <h1 className="icon"><span><i className="fa fa-lightbulb-o" aria-hidden="true"></i>
+                    </span>{correctAnswer}</h1>
+                </div>
             </div>
-            <div className="quiz__answer">
-                <h1 className="icon"><span><i className="fa fa-lightbulb-o" aria-hidden="true"></i>
-                </span>Kingston</h1>
-                <button className="btn-next">Next</button>
-            </div>
-        </div>
+        </div >
+
     )
 }
 
