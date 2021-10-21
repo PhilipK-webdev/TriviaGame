999108bd03fbaabca6d2aa64247f0875e6739287
PhilipK-webdev <philipk179@gmail.com>
2021-10-21 10:33:16 +0300

    Updates

---
+++ precache-manifest.9f60a21bc6f7f1557a1626831805c780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee1b9f8d99d5afeb2ee888e9f8c3e10",
+    "url": "/TriviaGame/index.html"
+  },
+  {
+    "revision": "738d159b09fccafe5a8e",
+    "url": "/TriviaGame/static/css/main.78ee2679.chunk.css"
+  },
+  {
+    "revision": "83f327277c51e4f22f41",
+    "url": "/TriviaGame/static/js/2.de81288f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TriviaGame/static/js/2.de81288f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738d159b09fccafe5a8e",
+    "url": "/TriviaGame/static/js/main.edb562a8.chunk.js"
+  },
+  {
+    "revision": "4c6a2c239a5f658e2e5a",
+    "url": "/TriviaGame/static/js/runtime-main.069ea83d.js"
+  }
+]);
